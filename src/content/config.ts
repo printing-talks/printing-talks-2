5d09346114d22daa0c366bb008260ce29b50e3f2
Saleh <dev@printingtalks.ae>
2023-12-28 14:58:16 +0400

    WIP Setup the project and some components

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+// 2. Define your collection(s)
+const productCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.coerce.date(),
+    tags: z.array(z.string()),
+    img: z.string(),
+    img_alt: z.string().optional()
+  })
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  'products': productCollection,
+};
